9a53fe61d4378cac64ce28b52553783efa4dd2a2
Caio Henrique Azevedo Ferreira <caiolee01@hotmail.com>
2023-11-02 16:04:23 -0300

    Create day_0.php
    Hello World

---
+++ day_0.php
@@ -0,0 +1,8 @@
+// Read a line of input from stdin
+$input = trim(fgets(STDIN));
+
+// Print "Hello, World." on the first line
+echo "Hello, World.\n";
+
+// Print the value of the input variable on the second line
+echo $input;
